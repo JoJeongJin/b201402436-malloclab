e6134c82b950476cebee3bc52727a588b6e51a35
JJJ <apjojeongjin@cnu.ac.kr>
2017-12-09 00:59:28 +0900

    void *coalesce 함수 구현 완료

--- mm-implicit.c
+++ mm-implicit.c
@@ -160,3 +160,35 @@ static int aligned(const void *p) {
  */
 void mm_checkheap(int verbose) {
 }
+
+static void *coalesce(void *bp){
+
+	size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+	size_t size = GET_SIZE(HDRP(bp));
+
+	if(prev_alloc && next_alloc){
+		return bp;
+	}
+
+	else if(prev_alloc && !next_alloc){
+		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		PUT(HDRP(bp), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size, 0));
+	}
+
+	else if(!prev_alloc && next_alloc){
+		size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+		PUT(FTRP(bp), PACK(size,0));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size,0));
+		bp=PREV_BLKP(bp);
+	}
+
+	else{
+		size += GET_SIZE(HDRP(PREV_BLKP(bp)))+GET_SIZE(FTRP(NEXT_BLKP(bp)));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+		bp = PREV_BLKP(bp);
+	}
+	return bp;
+}
